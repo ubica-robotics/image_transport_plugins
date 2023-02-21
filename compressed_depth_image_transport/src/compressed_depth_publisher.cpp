9553d98d3ae969efaabf8a70dadf254e7112b406
Plexiglas <torge1995@hotmail.de>
2023-02-21 10:36:19 +0100

    Update compressed depth publisher
    Fixed parameter declaration in compressed_depth_publisher.cpp

--- compressed_depth_image_transport/src/compressed_depth_publisher.cpp
+++ compressed_depth_image_transport/src/compressed_depth_publisher.cpp
@@ -67,66 +67,76 @@ namespace compressed_depth_image_transport
 
         uint ns_len = node->get_effective_namespace().length();
         std::string param_base_name = base_topic.substr(ns_len);
+        if (param_base_name.length() > 0 && param_base_name[0] == '/')
+            param_base_name.erase(0, 1);  
+        
         std::replace(param_base_name.begin(), param_base_name.end(), '/', '.');
+        
+        std::string depth_or_color = param_base_name.substr(0, param_base_name.find("."));
+        if (depth_or_color == "color") {
+            return;
+        }  
+        
         param_base_name += ".compressedDepth";
-
         std::string png_level_param_name = param_base_name + ".png_level";
-        rcl_interfaces::msg::ParameterDescriptor png_level_description;
-        png_level_description.name = "png_level";
-        png_level_description.type = rcl_interfaces::msg::ParameterType::PARAMETER_INTEGER;
-        png_level_description.description = "Compression level for PNG format";
-        png_level_description.read_only = false;
-        rcl_interfaces::msg::IntegerRange png_range;
-        png_range.from_value = 0;
-        png_range.to_value = 9;
-        png_range.step = 1;
-        png_level_description.integer_range.push_back(png_range);
-        try {
-            config_.png_level = node->declare_parameter(
-                    png_level_param_name, kDefaultPngLevel, png_level_description);
-        } catch (const rclcpp::exceptions::ParameterAlreadyDeclaredException &) {
-            RCLCPP_DEBUG(logger_, "%s was previously declared", png_level_param_name.c_str());
-            config_.png_level = node->get_parameter(png_level_param_name).get_value<int64_t>();
+        if (!node->has_parameter(png_level_param_name))
+        {
+            RCLCPP_WARN(logger_, "%s was not previously declared", png_level_param_name.c_str());
+            rcl_interfaces::msg::ParameterDescriptor png_level_description;
+            png_level_description.name = "png_level";
+            png_level_description.type = rcl_interfaces::msg::ParameterType::PARAMETER_INTEGER;
+            png_level_description.description = "Compression level for PNG format";
+            png_level_description.read_only = false;
+            rcl_interfaces::msg::IntegerRange png_range;
+            png_range.from_value = 0;
+            png_range.to_value = 9;
+            png_range.step = 1;
+            png_level_description.integer_range.push_back(png_range);
+            config_.png_level = node->declare_parameter(png_level_param_name, kDefaultPngLevel, png_level_description);
+        }
+        else {
+            RCLCPP_WARN(logger_, "%s was previously delared", png_level_param_name.c_str());
         }
 
         std::string depth_max_param_name = param_base_name + ".depth_max";
-        rcl_interfaces::msg::ParameterDescriptor depth_max_description;
-        depth_max_description.name = "depth_max";
-        depth_max_description.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
-        depth_max_description.description = "Maximum depth value (meter)";
-        depth_max_description.read_only = false;
-        rcl_interfaces::msg::FloatingPointRange depth_max_range;
-        depth_max_range.from_value = 0.0;
-        depth_max_range.to_value = 100.0;
-        depth_max_range.step = 0.01;
-        depth_max_description.floating_point_range.push_back(depth_max_range);
-        try {
-            config_.depth_max = node->declare_parameter(
-                    depth_max_param_name, kDefaultDepthMax, depth_max_description);
-        } catch (const rclcpp::exceptions::ParameterAlreadyDeclaredException &) {
-            RCLCPP_DEBUG(logger_, "%s was previously declared", depth_max_param_name.c_str());
-            config_.depth_max = node->get_parameter(depth_max_param_name).get_value<double>();
+        if (!node->has_parameter(depth_max_param_name))
+        {
+            RCLCPP_WARN(logger_, "%s was not previously declared", depth_max_param_name.c_str());
+            rcl_interfaces::msg::ParameterDescriptor depth_max_description;
+            depth_max_description.name = "depth_max";
+            depth_max_description.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
+            depth_max_description.description = "Maximum depth value (meter)";
+            depth_max_description.read_only = false;
+            rcl_interfaces::msg::FloatingPointRange depth_max_range;
+            depth_max_range.from_value = 0.0;
+            depth_max_range.to_value = 100.0;
+            depth_max_range.step = 0.01;
+            depth_max_description.floating_point_range.push_back(depth_max_range);
+            config_.depth_max = node->declare_parameter(depth_max_param_name, kDefaultDepthMax, depth_max_description);
+        }
+        else {
+            RCLCPP_WARN(logger_, "%s was previously delared", png_level_param_name.c_str());
         }
 
         std::string depth_quantization_param_name = param_base_name + ".depth_quantization";
-        rcl_interfaces::msg::ParameterDescriptor depth_quantization_description;
-        depth_quantization_description.name = "depth_quantization";
-        depth_quantization_description.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
-        depth_quantization_description.description = "Maximum depth value (meter)";
-        depth_quantization_description.read_only = false;
-        rcl_interfaces::msg::FloatingPointRange depth_quantization_range;
-        depth_quantization_range.from_value = 0.0;
-        depth_quantization_range.to_value = 150.0;
-        depth_quantization_range.step = 0.01;
-        depth_quantization_description.floating_point_range.push_back(depth_quantization_range);
-        try {
-            config_.depth_quantization = node->declare_parameter(
-                    depth_quantization_param_name, KDefaultDepthQuantization, depth_quantization_description);
-        } catch (const rclcpp::exceptions::ParameterAlreadyDeclaredException &) {
-            RCLCPP_DEBUG(logger_, "%s was previously declared", depth_quantization_param_name.c_str());
-            config_.depth_quantization = node->get_parameter(depth_quantization_param_name).get_value<double>();
+        if (!node->has_parameter(depth_quantization_param_name))
+        {
+            RCLCPP_WARN(logger_, "%s was not previously declared", depth_quantization_param_name.c_str());
+            rcl_interfaces::msg::ParameterDescriptor depth_quantization_description;
+            depth_quantization_description.name = "depth_quantization";
+            depth_quantization_description.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
+            depth_quantization_description.description = "Maximum depth value (meter)";
+            depth_quantization_description.read_only = false;
+            rcl_interfaces::msg::FloatingPointRange depth_quantization_range;
+            depth_quantization_range.from_value = 0.0;
+            depth_quantization_range.to_value = 150.0;
+            depth_quantization_range.step = 0.01;
+            depth_quantization_description.floating_point_range.push_back(depth_quantization_range);
         }
-
+        else {
+            RCLCPP_WARN(logger_, "%s was previously delared", png_level_param_name.c_str());
+        } 
+        
         //node->get_parameter_or<int>("png_level", config_.png_level, kDefaultPngLevel);
         //node->get_parameter_or<double>("depth_max", config_.depth_max, kDefaultDepthMax);
         //node->get_parameter_or<double>("depth_quantization", config_.depth_max, KDefaultDepthQuantization);
